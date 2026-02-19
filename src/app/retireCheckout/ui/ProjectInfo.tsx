cc179dfbdf2d7a1605452f7c16c8d006980aceda
Your Name~Francisco <franbalcarce21@gmail.com>
2026-02-19 11:22:26 -0300

    arreglois

--- src/app/retireCheckout/ui/ProjectInfo.tsx
+++ src/app/retireCheckout/ui/ProjectInfo.tsx
@@ -1,36 +1,31 @@
-import CountryFlag from "@/components/ProjectCard/CountryFlag";
-import React from "react";
-import { ProjectInfoProps } from "../types";
-import MethodologyInfo from "@/components/MethodologyInfo/MethodologyInfo";
+import CountryFlag from '@/components/ProjectCard/CountryFlag';
+import React from 'react';
+import { ProjectInfoProps } from '../types';
+import MethodologyInfo from '@/components/MethodologyInfo/MethodologyInfo';
 
-const ProjectInfo: React.FC<ProjectInfoProps> = ({
-  methodologyName,
-  project,
-  index,
-}) => {
+const ProjectInfo: React.FC<ProjectInfoProps> = ({ methodologyName, project, index }) => {
   return (
     <div className="flex items-center divide-x divide-gray-300">
+      {/* Ubicación */}
       <div className="flex items-center gap-2 px-3">
-        <p className="text-forestGreen font-neueMontreal font-medium">
-          Ubicación:
-        </p>
+        <p className="text-forestGreen font-neueMontreal font-medium">Ubicación:</p>
         <div className="flex items-center gap-2">
           <CountryFlag country={project?.country} />
-          <span className="text-forestGreen font-aeonik font-medium">
-            {project?.country}
-          </span>
+          <span className="text-forestGreen font-aeonik font-medium">{project?.country ?? ''}</span>
         </div>
       </div>
 
+      {/* Vintage */}
       <div className="flex items-center px-3">
         <span className="text-forestGreen font-neueMontreal font-medium">
-          {typeof index === "number" ? project?.vintages[index] : ""}
+          {typeof index === 'number' ? (project?.vintages?.[index] ?? '') : ''}
         </span>
       </div>
 
+      {/* Methodology */}
       <div className="flex items-center px-3">
         <MethodologyInfo
-          methodology={project?.methodologies[0]?.name || ""}
+          methodology={project?.methodologies?.[0]?.name ?? ''}
           methodologyName={methodologyName}
           isTextWhite={false}
         />
